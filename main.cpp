207939dfb2616aa02a2f69ad2fd4a4ee27066b41
nirsinai7 <73178530+nirsinai7@users.noreply.github.com>
2020-11-04 17:25:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,32 +19,5 @@ bool dfs(int cur, bool gen) {
 
 int main() {
     cin >> t;
-    for (i = 0; i < t; i++) {
-        cin >> n >> m;
-
-        for (j = 0; j < n + 1; j++) {
-            g[j].clear();
-            visited[j] = false;
-        }
-
-        for (j = 0; j < m; j++) {
-            cin >> a >> b;
-            g[a].push_back(b);
-            g[b].push_back(a);
-        }
-
-        flag = true;
-        j = 1;
-        while (j < n + 1 && flag) {
-            if (!visited[j]) {
-                if (!dfs(j, true)) {
-                    flag = false;
-                }
-            }
-            j++;
-        }
-        cout << "Scenario #"<< i + 1 << ":" << endl;
-        if (flag) cout << "No suspicious bugs found!" << endl;
-        else cout << "Suspicious bugs found!" << endl;
-    }
+    cout << "hello";
 }
